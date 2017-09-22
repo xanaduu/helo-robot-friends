c92141b0393cc6eb87d4b7dd4c38fb350c82490b
kale-stew <kylieastewart@gmail.com>
2017-09-22 13:57:43 -0700

    friend_controller

---
+++ server/controller/friend_controller.js
@@ -0,0 +1,30 @@
+const robots = require('../models/robots');
+let id = 1;
+
+// equivalent to managing our friends like a cart of items ... 
+// maybe not best way to do it?
+// manipulate the array of 'friends' (from robots) associated with each req/res
+
+module.exports = {
+    add: (req, res, next) => {
+        const { id } = req.query.id;
+        const { friends } = req.session.user;
+        const index = friends.findIndex( friend => friend.id === id );
+        if (index === -1) {
+            const selectedFriend = friend.find( friend => friend.id == id);
+            friends.push(selectedFriend);
+        }
+        res.status(200).send( req.session.user + ' has been added a your friend!' );
+    },
+
+    delete: (req, res, next) => {
+        const { id } = req.query.id;
+        const { friends } = req.session.user;
+        if (selectedFriend) {
+            const i = friends.findIndex( friend => friend.id == id );
+            friends.splice(i, 1);
+        }
+        res.status(200).send( req.session.user + ' has removed a friend.' );
+    }
+
+};
