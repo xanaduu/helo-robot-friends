7233b07ed75a16aedbcb6c606e5a7c59f2d8a9b6
kale-stew <kylieastewart@gmail.com>
2017-09-22 12:04:11 -0700

    html

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,10 +8,10 @@ export default class Login extends Component {
         <div className="Background" >
         <div className="splash-screen">
           <div className="logo-main"></div>
-          <h1 className="helo">Helo</h1>
-          <a href={process.env.REACT_APP_LOGIN}>
-          <button className="login">Login / Register</button>
-          </a>
+            <h1 className="helo">Helo</h1>
+            <a href={process.env.REACT_APP_LOGIN}>
+              <button className="login">Login / Register</button>
+            </a>
           </div>
         </div>
       </div>
