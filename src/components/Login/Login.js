e4b154fd43f395afca3838abe52a9307593cbfea
Rachel Nobles <rachel.noble77@gmail.com>
2017-09-22 10:43:10 -0700

    basic auth0

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import './Login.css';
 
-class Login extends Component {
+export default class Login extends Component {
   render() {
     return (
       <div className="App">
         <div className="Background" >
-
-
         <div className="splash-screen">
           <div className="logo-main"></div>
           <h1 className="helo">Helo</h1>
-
+          <a href={process.env.REACT_APP_LOGIN}>
           <button className="login">Login / Register</button>
-
+          </a>
           </div>
-
-
         </div>
       </div>
     );
   }
 }
-
-export default Login;
